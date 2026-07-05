e447f1a7555760c2afd57899805122b4ebcc7af0
Kaushal Thakur <thakurkaushal2424@gmail.com>
2026-07-05 19:12:43 +0530

    Update CORS allowed origins for production

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://ai-guest-review-analyzer-frontend.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
